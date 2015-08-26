9d2d976ac818cf3e2ff0ae639738c658dfd1d5d0
Nick Marshall <nialbima@gmail.com>
2015-08-26 17:03:41 -0400

    MENU FIRES ON THE PAGE YAAAAS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,15 @@
-$(function(){
+// $(function(){
 
 //arrays and objects
-
-//salads_array[0] gives us the name of the array
 salads_array = [ "Salad",
   {item: "The Gates of Eden", price:10, has_meat:[false, ""], vegan: false, gluten_free: true},
   {item: "The Cheek Corea", price:8, has_meat:[true, "pork"], vegan: false, gluten_free: false}
-]
+];
 
 soups_array = [ "Soup",
   {item: "Gazpacho", price:8, has_meat:[false, ""], vegan: true, gluten_free: true},
   {item: "Watermelon Soup", price:8, has_meat:[false, ""], vegan: true, gluten_free: true}
-]
+];
 
 pizza_array = [ "Pizza",
   {item: "Brian DeParma", price:13, has_meat:[false, ""], vegan: false, gluten_free: false},
@@ -24,14 +22,14 @@ pizza_array = [ "Pizza",
   {item: "Grapeful Dead", price:17, has_meat:[false, ""], vegan: false, gluten_free: false},
   {item: "Jackie Green", price:16, has_meat:[false, ""], vegan: true, gluten_free: false},
   {item: "In Ricotta Da Vegan", price:18, has_meat:[false, ""], vegan: true, gluten_free: false}
-]
+];
 
 dessert_array = [ "Dessert",
   {item: "Apple Cider Caramel Dessert Pizza", price: 12, has_meat:[false, ""], vegan: false, gluten_free: false},
   {item: "Vegan Pistachio Ice Cream", price:6, has_meat:[false, ""], vegan: true, gluten_free: true},
   {item: "Hot Honey Sunday", price: 8, has_meat:[false, ""], vegan: false, gluten_free: true},
   {item: "Candied Orange Dessert Pizza", price:13, has_meat:[false, ""], vegan: false, gluten_free: false}
-]
+];
 
 drinks_array = [ "Drinks",
   {item: "Coffee", price: 3, has_meat:[false, ""], vegan: true, gluten_free: true},
@@ -39,29 +37,47 @@ drinks_array = [ "Drinks",
   {item: "Soda", price: 3, has_meat:[false, ""], vegan: true, gluten_free: true},
   {item: "Beer", price:6, has_meat:[false, ""], vegan: true, gluten_free: false},
   {item: "Wine", price:8, has_meat:[false, ""], vegan: false, gluten_free: true}
-]
+];
 
-function menu_maker(array, key_name) {
-  menu_library[key_name] = array;
+function menu_maker(array) {
+  menu_library.push(array);
   return menu_library;
+};
+
+function menu_builder() {
+  //load arrays into menu object
+  menu_maker(salads_array);
+  menu_maker(soups_array);
+  menu_maker(pizza_array);
+  menu_maker(dessert_array);
+  menu_maker(drinks_array);
+
+  menu_header = $("#menu-container");
+
+  //append all menu_items to div header
+  menu_library.forEach(function(element){
+    section_header = "<ul class=food-category>"+element[0]+"</ul>"
+    menu_header.append(section_header)
+
+      for(j = 1; j < element.length; j++) {
+        menu_item = "<li class=menu-item>"+element[j].item+"</li>";
+        $(".food-category").last().append(menu_item)
+      }
+  })
 }
 
-var menu_library = {};
-
-//load arrays into menu object
-menu_maker(salads_array, salads_array[0]);
-menu_maker(soups_array, soups_array[0]);
-menu_maker(pizza_array, pizza_array[0]);
-menu_maker(dessert_array, dessert_array[0]);
-menu_maker(drinks_array, drinks_array[0]);
-//console.log(menu_library)
-//console.log(menu_library.Salad[1].item)
+var menu_library = [];
 
 //necessary variables
 var new_order = {}
 var new_order_button = $("#new-order-button");
 var kitchen_fire_button = $("#kitchen-fire-button")
+var menu_item = $("li.menu-item")
 
+menu_item.on('click', function() {
+  //this.attr("class", "ordered-item");
+  console.log("fired")
+})
 
 new_order_button.on('click', function() {
   new_order_button.remove();
@@ -77,23 +93,5 @@ new_order_button.on('click', function() {
 
 
 
-});
-
-
-
-
-//key_name is a string
-
-// for each item in each menu array, add to menu (.push the object together, don't write it out)
 //
-//menu should look like this:
-//menu = {  [each salad offered], [each sandwich offered]}
-//where each of those elements is an object
-//
-//
-// from key_translator:
-// //dynamically generate page table
-// symbol_library.forEach(function(element) {
-//   var new_row = '<div class="row"><div class="command-column-cell">'+element.keyname+'</div><div class="symbol-column-cell">'+element.symbol+'</div></div>'
-//   $(".row").last().append(new_row)
-// })
+// });
