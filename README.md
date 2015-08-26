ea91df01331f1b222eaeea9ee00724ef7fc5e608
Nick Marshall <nialbima@gmail.com>
2015-08-26 11:47:56 -0400

    basic site architecture set up, HTML divs set

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a point of sale app!
---
+++ scripts/scripts.js
@@ -0,0 +1,25 @@
+// var an_order = {salads: [], sandwiches:[], drink: []}
+//
+// var calvin = an_order
+// //populate with data
+//
+// //THIS WILL NOT WORK YET
+//
+// //stores all salad details
+// salads_array = [
+//   {item: "caesar", price:100, has_meat: [true, "anchovies"], vegan: false, gluten_free: true},
+//   {item: "caprese", price:100, has_meat: [false, ""], vegan: false, gluten_free: true},
+//   {item: ""}
+// ]
+//
+// drinks_array = [
+//   //same deal
+// ]
+//
+// //
+// var new_order = an_order
+// for each item in each menu array, add to menu (.push the object together, don't write it out)
+//
+//menu should look like this:
+//menu = {  [each salad offered], [each sandwich offered]}
+//where each of those elements is an object
